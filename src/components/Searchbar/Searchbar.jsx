103559bf3c96055b5d8d12c7ca28753fb73c9547
Johan Suarez <johanc.suarez@hotmail.com>
2020-05-08 16:59:32 -0500

    Creada funcion para busqueda general en componente searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
 import Dropdown from '../../containers/Dropdown/Dropdown';
 import './Searchbar.css';
+import axios from 'axios';
 
 const options = [
   { id: 1, value: 'Medellín' },
@@ -13,10 +14,55 @@ const options = [
   { id: 7, value: 'Cúcuta' },
 ];
 
+
+
+function boton(){
+  return(<div>
+    Botón de prueba para los request (en consola se muestran los resultados)<button>Click</button>
+  </div>)
+}
+
+
+
+
+
 function Searchbar() {
 
+  
+  const busquedaGeneralHabitaciones=()=>{   
+   const urlBackendLambda="http://ec2-13-58-217-208.us-east-2.compute.amazonaws.com/api/rooms/search?";
+   const urlBackendNode="http://ec2-13-58-217-208.us-east-2.compute.amazonaws.com/api/rooms/search?";
+   const urlBackendPython="http://ec2-13-58-217-208.us-east-2.compute.amazonaws.com/api/rooms/search?"
+   var ciudad="MDE";
+   var checkin="2020-05-09";
+   var checkout="2020-06-10";
+   var datosLambda="";
+    axios.get(urlBackendLambda+'location='+ciudad+'&checkin='+checkin+'&checkout='+checkout)
+    .then(responseLambda => {
+      console.log(responseLambda.data);     
+      })
+    .catch(e => {
+        console.log(e);
+    })
+    axios.get(urlBackendNode+'location='+ciudad+'&checkin='+checkin+'&checkout='+checkout)
+    .then(responseNodeJs => {
+      console.log(responseNodeJs.data)
+    })
+    .catch(e => {
+      console.log(e);
+  })
+  axios.get(urlBackendPython+'location='+ciudad+'&checkin='+checkin+'&checkout='+checkout)
+    .then(responsePython => {
+      console.log(responsePython.data)
+    })
+    .catch(e => {
+      console.log(e);
+  })
+  }
+
   const destinationCallback = (destination) => {
-    console.log('Destination selected ->', destination)
+    console.log('Destination selected ->', destination);
+    
   }
 
   return (
@@ -32,6 +78,7 @@ function Searchbar() {
       <div className="date-container">
         <span>Calendary: inicial</span>
         <span>Calendary: final</span>
+       <button onClick={busquedaGeneralHabitaciones}>Test button</button>
       </div>
     </div>
   );
