a06dd970dc7d3b5e39339797e551488406d5a800
FredyArroyave12 <FredyArroyave@udea.edu.co>
2020-05-28 22:52:16 -0500

    checkin-checkout-precio

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import Raiting from '../Raiting/Raiting';
 import ImageVysor from '../ImageVysor/ImageVysor';
-
+import axios from 'axios';
 import './CardDetails.css';
 
-const CardDetails = ({ img, city, price, totalPrice, realState, realStateLogo, name, rating, services }) => {
+
+const CardDetails = ({ checkin,checkout,id, img, city, price, totalPrice, realState, realStateLogo, name, rating, services }) => {
+  
+  function getBaseUrl(agencyName) {
+    const urlBackendNode = "http://ec2-13-58-217-208.us-east-2.compute.amazonaws.com/api/booking";
+    const urlBackendPython = "http://ec2-34-195-214-219.compute-1.amazonaws.com:8000/rooms/booking";
+    const urlBackendLambda = "https://34ld77s309.execute-api.us-east-1.amazonaws.com/prod/booking";
+
+    switch(agencyName) {
+      case 'S_TEAM': return urlBackendPython;
+      case 'Arrendamientos njs': return urlBackendNode;
+      case 'Lambda Team': return urlBackendLambda;
+      default: return '----';
+    }
+  }
+
+function reservar(){
+  const email=document.getElementById('email').value;
+  const baseUrl = getBaseUrl(realState)
+  axios.post(baseUrl,{
+
+checkin,
+checkout,
+email: email,
+name: email,
+id_room: id
+
+    }) 
+    .then(res => {
+      alert("Su reserva ha sido realizada, revise su email.")
+});
+}
+
   return (
     <div className="card-details-container">
       <div className="card-details-first">
@@ -14,13 +46,18 @@ const CardDetails = ({ img, city, price, totalPrice, realState, realStateLogo, n
       </div>
       <div className="card-details-image">
         <ImageVysor width="100%" src={img} alt="demo" description>
-          <Raiting raiting={rating} size="3x" />
+          <Raiting  raiting={rating} size="2x"  />
         </ImageVysor>
       </div>
       <div className="card-details-right">
         <h2 className="card-details-description">Descripción</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ratione consequatur, provident tenetur pariatur minima qui eum, consectetur reprehenderit sapiente commodi debitis eligendi laudantium aperiam nobis sit quaerat! Ab, eveniet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam pariatur adipisci quaerat impedit ducimus, ipsa, earum numquam quidem quos eos assumenda obcaecati at repellendus sit consequuntur et mollitia in laboriosam.</p>
-        <h1>Precio: {price}$</h1>
+        <p>Esta habitación no posee descripción</p>
+        <h1>Precio: {totalPrice}</h1>
+        <br/>
+        <div id="reservar">
+        <input className="input" id="email"/>Ingrese su email para reservar
+        <br/>
+        <button className="mail-button" onClick={reservar}>Reservar</button></div>
       </div>
       <div className="card-details-services">
         <b>Servicios:</b>
