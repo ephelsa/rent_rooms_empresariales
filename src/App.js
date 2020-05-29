8cb5fea48b7db17ccd4cda867b11166ba87fd53b
Johan Suarez <johanc.suarez@hotmail.com>
2020-05-29 17:11:52 -0500

    comentada la linea de importar el login den app.js porque se daña el css cuando importo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom'
-import Login from './containers/login/login';
+
 import { Page404 } from './components/PageHandler/PageHandler';
 import Layout from './containers/Layout/Layout';
 import Main from './components/Main/Main'
 import Cards from './containers/CardResumeList/CardResumeList'
 import Details from './containers/CardDetailsRequest/CardDetailsRequest'
+//import Login from './containers/login/login'
 
 function App() {
   return (
-   
-      <Switch>
-          <Route exact path="/login" component={Login}  />
-         <Layout>
+    <Layout>
+      <Switch>     
         <Route exact path="/home" component={Main}  />
         <Route exact path="/search" component={Cards} />
         <Route path="/details/:codCity/:agencyName/:roomId/:roomName" component={Details} />
-        </Layout>
         <Route path="/404" component={Page404} />
 
         <Route path="/">
-          <Redirect to="/login" />
+          <Redirect to="/home" />
         </Route>
         <Route>
           <Redirect to="/404" />
         </Route>
     </Switch>
-
+  </Layout>
   );
 }
 
